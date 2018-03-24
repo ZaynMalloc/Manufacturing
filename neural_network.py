e6b9ac266175dd5bd48bab974f4667980df87afd
Zacahary Gacer <zacharygacer@yahoo.com>
2018-03-24 17:32:45 -0400

    Fixing bugs

--- neural_network.py
+++ neural_network.py
@@ -2,12 +2,13 @@ import xlrd
 
 workbook_bitcoin = xlrd.open_workbook('bitcoin_data.xlsx')
 workbook_video_games = xlrd.open_workbook('shippment_data.xlsx')
-workbook_shippment = xlrd.open_workbook('video_game_sales.xlsx')
+workbook_bitcoin = xlrd.open_workbook('video_game_sales.xlsx')
 
-time_bitcoin = sheet['A']
-price_bitcoin = sheet['B']
-time_video_games = sheet['A']
-sales_video_games = sheet['A']
-time_shippment = sheet['A']
-shipments_shippment = sheet['B']
+time_bitcoin = workbook_bitcoin['A']
+price_bitcoin = workbook_bitcoin['B']
+time_video_games = workbook_video_games['A']
+sales_video_games = workbook_video_games['A']
+time_shippment = workbook_shippment['A']
+shipments_shippment = workbook_shippment['B']
 
+print(time_bitcoin)
