6792ecd4857abb514330d40a3ca087de58e4eaab
Zacahary Gacer <zacharygacer@yahoo.com>
2018-03-24 15:54:30 -0400

    Changing import library

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
-import xlrd
+import openpyxl
 
-workbook_bitcoin = xlrd.open_workbook('bitcoin_data.xlsx')
-workbook_video_games = xlrd.open_workbook('shippment_data.xlsx')
-workbook_shippment = xlrd.open_workbook('video_game_sales.xlsx')
+workbook_bitcoin = openpyxl.load_workbook('bitcoin_data.xlsx')
+workbook_video_games = openpyxl.load_workbook('shippment_data.xlsx')
+workbook_shippment = openpyxl.load_workbook('video_game_sales.xlsx')
 
+print(workbook_bitcoin)
