befbda3eb291326e041d6e42b72f23cfb5d63d01
Zacahary Gacer <zacharygacer@yahoo.com>
2018-03-24 15:44:44 -0400

    Adding code for opneing workbooks and inserting workbooks

--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,6 @@
-import numpy
+import xlrd
 
-def NN(m1,m2,w1,w2,b):
-    z = m1 * w1 + m2 * w2 + b
-    return sigmoid(z)
+workbook_bitcoin = xlrd.open_workbook('bitcoin_data.xlsx')
+workbook_video_games = xlrd.open_workbook('shippment_data.xlsx')
+workbook_shippment = xlrd.open_workbook('video_game_sales.xlsx')
 
-def sigmoid(x):
-    return 1/(1 + numpy.exp(-x))
-
-w1 = numpy.random.randn()
-w2 = numpy.random.randn()
-b = numpy.random.randn()
-
-print(NN(3, 1.5, w1, w2, b))
