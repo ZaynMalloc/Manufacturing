79f5b2e0251e024691fa99cd8ff4d82a6371ccdd
Zacahary Gacer <zacharygacer@yahoo.com>
2018-03-25 21:41:21 -0400

    Adding learning rate and cost function

--- network.py
+++ network.py
@@ -2,40 +2,67 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 #Here is our input data [bitcoin price, video games sales]
-bitcoin_price_video_game_data = [[367.13,157], [436.61, 292.2], [415.16, 253], [449.33, 142], [531.8, 138],
-[672.48, 181.5], [625.88, 142], [572.33, 179], [608.44, 234.3], [697.37, 216], [742.01, 723.8],
-[968.23, 997], [967.67, 127], [1,190.89, 204], [1,079.75, 485], [1,349.19, 195],
-[2,328.91, 147], [2,504.28, 231], [2,873.83, 182], [4,764.87, 168],
-[4,349.29, 316], [4,353.05, 238], [9,916.54, 1147], [13,860.14, 1270]]
-
-shippment = [2537.66, 2705.605, 2730, 2764.293, 2858, 2978, 2997, 2977]
+data = [[415.16, 253, 2537.66], [672.48, 181.5,2705.605], 
+[608.44, 234.3,2730],[968.23, 997,2764.293],
+[1,079.75, 485, 2858], [2,504.28, 231, 2978],
+[4,764.87, 168, 2978],[4,349.29, 316, 2997], 
+[13,860.14, 1270, 2977]]
 
 #Sigmoid Function
 def sigmoid (x):
     return 1/(1 + np.exp(-x))
 
 #Derivative of Sigmoid Function
-def derivatives_sigmoid(x):
+def derivative_sigmoid(x):
     return x * (1 - x)
 
 #Scatter Data
 '''
-for i in range(len(bitcoin_price_video_game_data)):
-    point = bitcoin_price_video_game_data[i]
+for i in range(len(data)):
+    point = data[i]
     plt.scatter(point[0], point[1])
     plt.show()
 '''
-#Training loop
-for i in range(1,1000):
-    ri = np.random.randint(len(bitcoin_price_video_game_data))
-    point = bitcoin_price_video_game_data[ri]
 
-    z = bitcoin_price_video_game_data[0] * w1 + bitcoin_price_video_game_data[1] * w2 + b
+for i in range(len(data)):
+    point = data[i]
+
+#Training loop
+
+learning_rate = 0.001
+
+for i in range(10000):
+
+    w1 = np.random.randn()
+    w2 = np.random.randn()
+    b = np.random.randn()
+
+    ri = np.random.randint(len(data))
+    point = data[ri]
+  
+    z = point[0] * w1 + point[1] * w2 + b
     pred = sigmoid(z)
 
     target = point[2]
     cost = np.square(pred - target)
 
+    if i % 1000 == 0:
+        print(cost)
+
+    dcost_pred = 2 * (pred - target)
+    dpred_dz = derivative_sigmoid(z)
+    dz_dwl = point[0]
+    dz_dw2 = point[1]
+    dz_db = 1
+
+    dcost_dwz = dcost_pred * dpred_dz 
+
+    dcost_dw1 = dcost_pred * dz_dwl
+    dcost_dw2 = dcost_pred * dz_dw2
+    dcost_db = dcost_dwz * dz_db
+ 
+    w1 = w1 - learning_rate * dcost_dw1
+    w2 = w2 - learning_rate * dcost_dw2
 
     print(point)
     print(cost)
